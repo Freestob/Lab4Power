5c0e5a36e219e84ddf179afaf7e67d0e3df8a660
Bradley Freestone <bradley.freestone@gmail.com>
2018-07-18 15:37:36 -0400

    Got the main thing down

---
+++ Lab4Power/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab4Power
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter in a number.");
+            
+            int x = int.Parse(Console.ReadLine());
+           
+            
+            for (int i = 0; i < x; i++)
+            {
+                int square = i * i;
+                int cubed = i * i * i;
+                Console.WriteLine("{0} {1} {2}" , i, square, cubed);
+            }
+            Console.ReadKey();
+
+        }
+       
+        
+    }
+}
